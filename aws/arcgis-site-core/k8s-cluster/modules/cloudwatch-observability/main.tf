f2894fe66d785d0f225a3244cabfc586bf44e61e
Pavel Bobov <pbobov@esri.com>
2024-07-02 17:14:01 -0700

    Added Esri copyright to sources

--- aws/arcgis-site-core/k8s-cluster/modules/cloudwatch-observability/main.tf
+++ aws/arcgis-site-core/k8s-cluster/modules/cloudwatch-observability/main.tf
@@ -6,20 +6,19 @@
  * See: https://docs.aws.amazon.com/AmazonCloudWatch/latest/monitoring/Container-Insights-setup-EKS-addon.html
  */
 
-/* Copyright 2024 Esri
- *
- * Licensed under the Apache License Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+# Copyright 2024 Esri
+#
+# Licensed under the Apache License Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 # Install the Amazon CloudWatch Observability EKS add-on.
 resource "aws_eks_addon" "cloudwatch_observability" {

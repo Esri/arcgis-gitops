654d5d0384ed36d7fba74ffd8fcbb11b6efdbf4a
Pavel Bobov <pbobov@esri.com>
2024-07-02 17:01:18 -0700

    Added Esri copyright to sources

--- aws/arcgis-server-linux/backup/main.tf
+++ aws/arcgis-server-linux/backup/main.tf
@@ -20,6 +20,20 @@
  * The module retrieves the backup S3 bucket name from '/arcgis/${var.site_id}/s3/backup' SSM parameters.
  */
 
+# Copyright 2024 Esri
+#
+# Licensed under the Apache License Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License. 
+ 
 terraform {
   backend "s3" {
     key = "terraform/arcgis-enterprise/arcgis-server/backup.tfstate"
